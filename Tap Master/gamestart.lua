ae6ba31159e5ecfddfd3c8adbb1b2183badbd83c
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-02-25 16:49:24 -0600

    tap master code started

---
+++ Tap Master/gamestart.lua
@@ -0,0 +1,103 @@
+local composer = require( "composer" )
+local widget = require("widget")
+
+local function handlePlay(event)
+	if(event.phase == "ended") then
+		print("now move the scene to next scene")
+		local options = {effect = "fade", time = 800}
+		composer.gotoScene("game", options)
+		print("should have moved")
+	end
+end
+
+local scene = composer.newScene()
+ 
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+ 
+ 
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+ 
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+end
+ 
+ 
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+        local background = display.newImage("background.jpeg")
+		background.x = display.contentCenterX
+		background.y = display.contentCenterY
+		sceneGroup:insert(background)
+		local play = widget.newButton(
+		{
+			left = 50,
+			top = 50,
+			id = "playButton",
+			onEvent = handlePlay,
+			defaultFile = "rsz_playbutton.png",
+			overFile = "rsz_playbutton.png"
+
+		})
+		play.x = display.contentCenterX
+		play.y = display.contentCenterY
+		sceneGroup:insert(play)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+ 
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
